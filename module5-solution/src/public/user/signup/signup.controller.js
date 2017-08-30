c9298aed88cb24a244952b3c778332ee1291111c
Luiz <luiz.cabral@usp.br>
2017-08-30 14:05:46 -0300

    Assigment 5 v1.0

---
+++ module5-solution/src/public/user/signup/signup.controller.js
@@ -0,0 +1,28 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.controller('RegistrationController', RegistrationController);
+
+RegistrationController.$inject = ['UserService'];
+function RegistrationController(UserService) {
+  var $ctrl = this;
+  $ctrl.user = {}
+
+  $ctrl.validateFavDish = function (nextKey) {
+  	UserService.exist($ctrl.user.favDish + nextKey.key).then(function (response) {
+      	$ctrl.invalidFavDish = false;
+    }, function(response) {
+    	$ctrl.invalidFavDish = true;
+    });
+  }
+
+  $ctrl.submit = function () {
+    $ctrl.validateFavDish("");
+  	UserService.signup($ctrl.user);
+    $ctrl.saved = true;
+  };
+}
+
+
+})();
